b11cb8b14541d22bc49fec5f1878ac32e196d4cd
nguyenminhthong2020 <1612674@student.hcmus.edu.vn>
2021-11-10 17:10:31 +0700

    infinity scroll / flatlist

--- DemoApp/src/Containers/ExampleContainer.js
+++ DemoApp/src/Containers/ExampleContainer.js
@@ -8,6 +8,7 @@ import {
   Pressable,
   TouchableOpacity,
   ScrollView,
+  FlatList,
 } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
@@ -15,50 +16,44 @@ import { Brand } from '@/Components'
 import { useTheme } from '@/Hooks'
 import { useLazyFetchOneQuery } from '@/Services/modules/users'
 import { changeTheme } from '@/Store/Theme'
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import { color } from 'react-native-reanimated'
-import { getImagesAsync, testDispatch} from '@/Store/Images'
-import FastImage from 'react-native-fast-image';
+import { getImagesAsync } from '@/Store/Images'
+import FastImage from 'react-native-fast-image'
 
 const ExampleContainer = () => {
   const { t } = useTranslation()
   const { Common, Fonts, Gutters, Layout } = useTheme()
   const dispatch = useDispatch()
   const isDarkMode = useSelector(state => state.theme.darkMode)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const listImages = useSelector(state => state.images)
+  const arrUri = listImages.map(i => i.src.original)
+  // console.log(arrUri);
+  console.log('currentPage ' + currentPage)
 
   useEffect(() => {
-     dispatch(getImagesAsync({
-       per_page: 8,
-       page:1
-     }))
+    dispatch(
+      getImagesAsync({
+        per_page: 10,
+        page: 1,
+      }),
+    )
   }, [])
-  const listImages = useSelector(state => state.images);
-  const arrUri = listImages.map(i => i.src.original);
-  //alert(listImages[0].src.original);
-  // list image
-  const showListImage = () => {
-    return arrUri.map((i, index) =>
-      <View key={index} style={{marginHorizontal: '10%', marginBottom: 15, borderWidth: 2, borderColor: isDarkMode?'yellow':'black'}}>
-      <Pressable onPress={()=>alert(`Ảnh thứ ${index}`)}>
-      <FastImage 
-              style={{width: '100%', height: 200, borderRadius: 0}}
-              resizeMode={FastImage.resizeMode.cover}
-              source={{
-                uri: i,
-                priority: FastImage.priority.normal,
-              }}
-            />
-            </Pressable>
-            </View>)
-    {/* <Text style={{color: 'red'}}>{`${i.src.original}?auto=compress&cs=tinysrgb&h=180`}
-    </Text>) */}
-  }
+
+  useEffect(() => {
+    dispatch(
+      getImagesAsync({
+        per_page: 10 * currentPage,
+        page: 1,
+      }),
+    )
+  }, [currentPage, dispatch])
 
   const [userId, setUserId] = useState('9')
-  const [
-    fetchOne,
-    { data, isSuccess, isLoading, isFetching, error },
-  ] = useLazyFetchOneQuery()
+  const [fetchOne, { data, isSuccess, isLoading, isFetching, error }] =
+    useLazyFetchOneQuery()
 
   useEffect(() => {
     fetchOne(userId)
@@ -68,76 +63,103 @@ const ExampleContainer = () => {
     dispatch(changeTheme({ theme, darkMode }))
   }
 
+  const loadMoreItem = () => {
+    console.log('mới nè: ', currentPage + 1)
+    setCurrentPage(currentPage + 1)
+  }
+
+  const renderItem = ({ item }) => {
+    return (
+      <View
+        style={{
+          height: 170,
+          marginHorizontal: '10%',
+          marginBottom: 25,
+          borderWidth: 2,
+          borderColor: isDarkMode ? 'yellow' : 'black',
+        }}
+      >
+        <Pressable onPress={() => alert(`Ảnh ${item}`)}>
+          {/* <Text style={{ color: 'red' }}>{`${item}?auto=compress&cs=tinysrgb&h=170`}</Text> */}
+          <FastImage
+            style={{ width: '100%', height: 170, borderRadius: 0 }}
+            resizeMode={FastImage.resizeMode.cover}
+            source={{
+              uri: `${item}?auto=compress&cs=tinysrgb&h=170`,
+              priority: FastImage.priority.normal,
+            }}
+          />
+        </Pressable>
+      </View>
+    )
+  }
+
+  const renderFooter = () => {
+    return (
+      <View style={{ marginVertical: 16 }}>
+        <ActivityIndicator size="large" color="aaa" />
+      </View>
+    )
+  }
+
+  const showListImage = () => {
+    return (
+      <FlatList
+        getItemLayout={(_, index) => ({
+          length: 200,
+          offset: 200 * index,
+          index,
+        })}
+        removeClippedSubviews={true}
+        windowSize={5}
+        style={{ marginBottom: 120, marginTop: 10 }}
+        showsVerticalScrollIndicator={true}
+        initialNumToRender={10}
+        data={arrUri}
+        keyExtractor={i => i}
+        renderItem={renderItem}
+        ListFooterComponent={renderFooter}
+        onEndReached={() => loadMoreItem()}
+        onEndReachedThreshold={0.5}
+      />
+    )
+  }
+
   return (
-    <ScrollView
-      // style={Layout.fill}
-      // contentContainerStyle={[
-      //   Layout.fill,
-      //   Layout.colCenter,
-      //   Gutters.smallHPadding,
-      // ]}
-    >
-      {/* <View style={[[Layout.colCenter, Gutters.smallHPadding]]}>
-        <Brand />
-        {(isLoading || isFetching) && <ActivityIndicator />}
-        {!isSuccess ? (
-          <Text style={Fonts.textRegular}>{error}</Text>
-        ) : (
-          <Text style={Fonts.textRegular}>
-            {t('example.helloUser', { name: data?.name })}
-          </Text>
-        )}
-      </View> */}
-      {/* <View
+    <View>
+      <Text
         style={[
-          Layout.row,
-          Layout.rowHCenter,
-          Gutters.smallHPadding,
-          Gutters.largeVMargin,
-          Common.backgroundPrimary,
+          Fonts.textRegular,
+          Gutters.smallBMargin,
+          { marginTop: 1, marginLeft: '10%' },
         ]}
       >
-        <Text style={[Layout.fill, Fonts.textCenter, Fonts.textSmall]}>
-          {t('example.labels.userId')}
-        </Text>
-        <TextInput
-          onChangeText={setUserId}
-          editable={!isLoading}
-          keyboardType={'number-pad'}
-          maxLength={1}
-          value={userId}
-          selectTextOnFocus
-          style={[Layout.fill, Common.textInput]}
-        />
-      </View> */}
-      <Text style={[Fonts.textRegular, Gutters.smallBMargin, {marginTop: 1, marginLeft: '10%'}]}>DarkMode :</Text>
-      <View style={{flexDirection: 'row', marginLeft: '10%'}}>
-      <TouchableOpacity
-        style={[Common.button.rounded, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: null })}
-      >
-        <Text style={Fonts.textRegular}>Auto</Text>
-      </TouchableOpacity>
+        DarkMode :
+      </Text>
+      <View style={{ flexDirection: 'row', marginLeft: '10%' }}>
+        <TouchableOpacity
+          style={[Common.button.rounded, Gutters.regularBMargin]}
+          onPress={() => onChangeTheme({ darkMode: null })}
+        >
+          <Text style={Fonts.textRegular}>Auto</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        style={[Common.button.outlineRounded, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: true })}
-      >
-        <Text style={Fonts.textRegular}>Dark</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={[Common.button.outlineRounded, Gutters.regularBMargin]}
+          onPress={() => onChangeTheme({ darkMode: true })}
+        >
+          <Text style={Fonts.textRegular}>Dark</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        style={[Common.button.outline, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: false })}
-      >
-        <Text style={Fonts.textRegular}>Light</Text>
-      </TouchableOpacity>
-
+        <TouchableOpacity
+          style={[Common.button.outline, Gutters.regularBMargin]}
+          onPress={() => onChangeTheme({ darkMode: false })}
+        >
+          <Text style={Fonts.textRegular}>Light</Text>
+        </TouchableOpacity>
       </View>
-      <View style={{flexDirection: 'column', marginTop: 15}}>
       {showListImage()}
-      </View>
-     </ScrollView>
+    </View>
   )
 }
 
